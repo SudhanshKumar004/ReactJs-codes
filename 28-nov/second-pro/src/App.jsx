58aadb0d16d74d28b33b71bab03e830b57070ed1
Sudhansh Kumar <sudhanshkb04@gmail.com>
2024-11-29 12:45:53 +0530

    variable in jsx

--- 28-nov/second-pro/src/App.jsx
+++ 28-nov/second-pro/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import { createElement, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
+  let name = "Sudhansh"
+  let obj ={
+    city : "Bhopal",
+    age : 24
+  }
   return (
-    <h1>Hello From app</h1>
+    <>
+      <h1>Name is {name}</h1>
+
+      <h2>{obj.age}</h2>
+      <h2>{obj.city}</h2>
+    </>
   )
 }
 
