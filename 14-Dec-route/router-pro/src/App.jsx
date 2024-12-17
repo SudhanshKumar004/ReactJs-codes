7042294661232c6494af7d3c4558306ad10b2fe4
Sudhansh Kumar <sudhanshkb04@gmail.com>
2024-12-17 12:34:37 +0530

    added outlet

--- 14-Dec-route/router-pro/src/App.jsx
+++ 14-Dec-route/router-pro/src/App.jsx
@@ -11,12 +11,13 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/service" element={<Service/>}/>
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/service" element={<Service/>}/>
+        </Route>
       </Routes>
-    <Layout/>
     </>
   )
 }
--- 14-Dec-route/router-pro/src/Layout.jsx
+++ 14-Dec-route/router-pro/src/Layout.jsx
@@ -1,19 +1,67 @@
-import { Link } from 'react-router-dom'
-
+import { Link,Outlet } from 'react-router-dom'
 function Layout(){
     return(
         <>
-        <section className="Navbar">
-            <nav>
-                {/* <h1>Logo</h1> */}
-                <ul style={{display:"flex",gap:"60px",listStyle:"none",cursor:"pointer"}}>
-                    <li><Link to="/home">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                    <li><Link to="/service">Service</Link></li>
-                </ul>
-            </nav>
-        </section>
+        <header>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item" style={{display:"flex"}}>
+          <li className="nav-link active"> <Link to="/"> Home </Link></li>
+          <li className="nav-link active"><Link to="/about"> About </Link></li>
+          <li className="nav-link active"><Link to="/contact"> Contact </Link></li>
+          <li className="nav-link active"><Link to="/service"> Service </Link></li>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+        </header>
+
+        <main>
+            <Outlet />
+        </main>
+
+        <footer style={{display:"flex",alignItems:"center",gap:"40px"}}>
+            <ul>
+              <li>Home</li>    
+              <li>About</li>    
+              <li>Contact</li>    
+              <li>Service</li>    
+            </ul>
+            <h1 style={{fontSize:"20px"}}>All Copyrights Reserved 2024</h1>
+            <ul>
+                <li>Instagram</li>
+                <li>FaceBook</li>
+                <li>Twitter</li>
+            </ul>
+        </footer>
         </>
     )
 }
