7c3e2cfaf420a2592248e7d98fb17f20b7a58d9c
Sudhansh Kumar <sudhanshkb04@gmail.com>
2024-12-25 21:31:54 +0530

    form condition with useState

--- 24-Dec-form/formPro/src/Form.jsx
+++ 24-Dec-form/formPro/src/Form.jsx
@@ -6,21 +6,29 @@ const Frm = () => {
     let[h,setH] = useState('')
     let[c,setC] = useState('')
     let[b,setB] = useState(false)
+    let[d,setD] = useState(false)
 
     function hinput(event){
         console.log(event.target.value)
         setH(event.target.value)
     }
     function ab(){
-        setB(true)
+        setB(!(b))
+        setD(!(d))
+    }
+
+    function inp2(i)
+    {
+        setC(i.target.value)
+
     }
 
     return(
         <>
         <h1>Name is {b ? h : '' }</h1>
-        {/* <h2>Work is {c}</h2> */}
-        <input type="text" onChange={hinput} /><br />
-        {/* <input type="text" onChange={inp2} /> */}
+        <h1>Work is {d ? c : '' }</h1>
+        <input type="text" onChange={hinput} placeholder="name" /><br />
+        <input type="text" onChange={inp2} placeholder="work" /><br />
         <button onClick={ab}>Submit</button>
         </>
     )
