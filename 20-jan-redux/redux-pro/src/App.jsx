fd0804bbc334e72fd7dbad61fec1dfb5010a30c9
Sudhansh Kumar <sudhanshkb04@gmail.com>
2025-01-21 12:28:26 +0530

    Redux Store

--- 20-jan-redux/redux-pro/src/App.jsx
+++ 20-jan-redux/redux-pro/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const App = () => {
   return (
@@ -9,4 +10,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App0
